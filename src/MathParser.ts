a498ef0b74a86932502b800b0e99dfaa7fb32f96
DaniCatGames <dnazarkin@outlook.com>
2025-06-17 23:15:51 +0200

    some more main class additions

--- src/MathParser.ts
+++ src/MathParser.ts
@@ -4,6 +4,7 @@ import { Simplifier } from "./Simplification/Simplifier";
 import { Node } from "./Typescript/Node";
 import { Evaluator } from "./Solving/Evaluator";
 import { Function, FunctionWithoutDerivative } from "./Math/Symbolic/MathFunctions";
+import { BasicNodes } from "./Node/BasicNodes";
 
 type Equation = Node;
 
@@ -14,7 +15,7 @@ export class MathParser {
 	evaluator: Evaluator;
 
 	functions: Function[] = [];
-	variables: { [variable: string]: number | undefined } = {};
+	variables: { [variable: string]: number } = {};
 	constants: { [variable: string]: number } = {};
 
 	constructor(parserConfig?: Partial<ParserConfig>) {
@@ -24,13 +25,33 @@ export class MathParser {
 		this.evaluator = new Evaluator([], {});
 	}
 
+	addVariables(...variables: ([string, number] | string)[]) {
+		variables.forEach((variable) => {
+			if(typeIs(variable, "string")) {
+				this.variables[variable] = 0;
+				this.parser.addVariable(variable);
+			} else {
+				this.variables[variable[0]] = variable[1];
+				this.parser.addVariable(variable[0]);
+			}
+		});
+	}
+
 	addFunctions(...functions: (Function | FunctionWithoutDerivative)[]) {
 		functions.forEach(func => {
+			let newFunc;
 			if("derivative" in func) {
-
+				newFunc = func;
 			} else {
-
+				newFunc = {
+					...func,
+					derivative: () => BasicNodes.Zero(),
+				};
 			}
+
+			this.functions.push(newFunc);
+			this.parser.addFunction(newFunc);
+			this.evaluator.addFunction(newFunc);
 		});
 	}
 }
